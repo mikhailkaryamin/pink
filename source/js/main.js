7961104c6bffe54cdd8cca6836518ef67d67b00c
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-01-09 17:55:00 +0500

    mobileIndex

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,12 @@
-let mainNav = document.querySelector('.main-nav');
-let mainNavButton = document.querySelector('.main-nav__toggle');
+let pageHeaderWrapperEl = document.querySelector('.page-header__wrapper');
+let mainNavEl = document.querySelector('.main-nav');
+let mainNavButtonEl = document.querySelector('.main-nav__toggle');
 
-mainNav.classList.remove('main-nav--nojs');
-mainNav.classList.add('main-nav--opened');
+mainNavEl.classList.remove('main-nav--nojs');
+mainNavEl.classList.add('main-nav--opened');
 
-mainNavButton.addEventListener('click', function() {
-  mainNav.classList.toggle('main-nav--opened');
-  mainNav.classList.toggle('main-nav--closed');
+mainNavButtonEl.addEventListener('click', function() {
+  pageHeaderWrapperEl.classList.toggle('page-header__wrapper--closed-menu')
+  mainNavEl.classList.toggle('main-nav--opened');
+  mainNavEl.classList.toggle('main-nav--closed');
 })
