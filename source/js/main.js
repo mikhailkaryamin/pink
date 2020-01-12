69fe710a7562bde242ab97cd3cd901aadbd27fea
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-01-12 17:59:30 +0500

    fix opened menu

--- source/js/main.js
+++ source/js/main.js
@@ -3,10 +3,11 @@ let mainNavEl = document.querySelector('.main-nav');
 let mainNavButtonEl = document.querySelector('.main-nav__toggle');
 
 mainNavEl.classList.remove('main-nav--nojs');
-mainNavEl.classList.add('main-nav--opened');
+mainNavEl.classList.add('main-nav--closed');
+pageHeaderWrapperEl.classList.toggle('page-header__wrapper--closed-menu');
 
 mainNavButtonEl.addEventListener('click', function() {
-  pageHeaderWrapperEl.classList.toggle('page-header__wrapper--closed-menu')
+  pageHeaderWrapperEl.classList.toggle('page-header__wrapper--closed-menu');
   mainNavEl.classList.toggle('main-nav--opened');
   mainNavEl.classList.toggle('main-nav--closed');
 })
